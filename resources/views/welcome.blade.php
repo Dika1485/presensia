dd2a4182aa106edc1820c5aef54fc59d90efe2fc
stephenprasetyachrismawan <80116750+stephenprasetyachrismawan@users.noreply.github.com>
2023-03-15 19:10:18 +0700

    hbx

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -871,7 +871,7 @@
                                 </svg>
                             </div>
 
-                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Foto</h2>
+                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Video</h2>
 
                             <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
                                 Laravel has wonderful documentation covering every aspect of the framework. Whether you
