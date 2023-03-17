5c69e1bea33eea6eeb1f19d5f9ddde6f2c2fe0a0
stephenprasetyachrismawan <80116750+stephenprasetyachrismawan@users.noreply.github.com>
2023-03-17 15:16:24 +0700

    daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,26 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php', './storage/framework/views/*.php', './resources/views/**/*.blade.php', "./src/**/*.{html,js}"
+
     ],
+
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+                sans: [
+                    'Figtree',
+                    ... defaultTheme.fontFamily.sans
+                ]
+            }
+        }
     },
 
     plugins: [
-        [require('@tailwindcss/forms')],
-        [require('daisyui')]
+        [require('daisyui'), require('@tailwindcss/forms')]
+
     ]
 };
