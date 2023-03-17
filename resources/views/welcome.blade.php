2c107b9008b74dad303f5943ab19f11e2f79a4ef
irfanp22 <106128747+irfanp22@users.noreply.github.com>
2023-03-17 11:12:07 +0700

    daisyui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1002,6 +1002,7 @@
                         </a>
                     </div>
                 </div>
+                <button class="btn btn-primary">One</button>
 
                 <div class="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
                     Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
